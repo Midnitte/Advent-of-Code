e1100592aab1af3b73bccf5e34814373d875eaba
Midnitte <midnitte@gmail.com>
2022-12-02 16:55:36 -0500

    Day 1 (2022) Solution

--- 2022/Day1.py
+++ 2022/Day1.py
@@ -4,4 +4,15 @@ import os, sys
 with open(os.path.join(sys.path[0], 'Day1input.txt'), 'r') as file:
     data = file.read()
 
-print(data)
+# Part 1
+data = data.split('\n\n')
+
+data = [list(x.split()) for x in data]
+
+data = pd.DataFrame(data)
+data = data.astype(float)
+data['Sum'] = data.sum(axis=1, numeric_only=True)
+print(data['Sum'].nlargest(n=1))
+
+# Part 2
+
