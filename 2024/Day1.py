0364d184ed5c865ae467c3952d0b0eefd83a6519
Chris Norulak <midnitte@gmail.com>
2024-12-01 15:20:23 -0500

    2024 Day 1 solution

---
+++ 2024/Day1.py
@@ -0,0 +1,31 @@
+# Part 1
+from pathlib import Path
+from pprint import pprint as print
+
+path = Path.cwd() / "2024/Day1input.txt"
+content = path.read_text(encoding="utf-8")
+
+firstColumn = sorted([int(line.split()[0]) for line in content.splitlines() if line.strip()])
+secondColumn = sorted([int(line.split()[1]) for line in content.splitlines() if line.strip()])
+
+def distanceCalc(r1, r2):
+    return abs(r1-r2)
+
+total = 0
+for index, _ in enumerate(firstColumn):
+    total += distanceCalc(firstColumn[index], secondColumn[index])
+
+
+print(total)
+
+# Part 2
+## Similarity Scores
+
+def simScore(r1, r2):
+    return r1 * r2.count(r1)
+
+similarScore = 0
+for index, _ in enumerate(firstColumn):
+    similarScore += simScore(firstColumn[index], secondColumn)
+
+print(similarScore)
