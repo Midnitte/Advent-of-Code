64eadaa88de1e58ebad3156fa7b44b538bd05fad
Chris Norulak <49769850+Midnitte@users.noreply.github.com>
2025-12-01 12:34:35 -0500

    Initial 2025 Day 1

---
+++ 2025/Day1.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from pprint import pprint as print
+
+path = Path.cwd() / "2025/Day1input.txt"
+content = path.read_text(encoding="utf-8")
+
+directions = [line[0] for line in content.splitlines()]
+magnitudes = [int(line[1:]) for line in content.splitlines()]
+
+results = [50]
+
+for id, result in enumerate(directions):
+    if result == "R":
+        results.append(results[-1] + magnitudes[id])
+    else:
+        results.append(results[-1] - magnitudes[id])
+
+print(results.count(0))
