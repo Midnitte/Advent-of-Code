0fcf417e46365bfb59015071c6c53c0220a26de1
Chris Norulak <49769850+Midnitte@users.noreply.github.com>
2025-12-02 12:28:50 -0500

    debugging

--- 2025/Day1.py
+++ 2025/Day1.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 from pprint import pprint as print
 
-path = Path.cwd() / "2025/Day1input.txt"
-content = path.read_text(encoding="utf-8")
+# path = Path.cwd() / "2025/Day1input.txt"
+# content = path.read_text(encoding="utf-8")
+content = "L68\nL30\nR48\nL5\nR6\nL5\nL1\nL99\nR14\nL82"
 
 directions = [line[0] for line in content.splitlines()]
 magnitudes = [int(line[1:]) for line in content.splitlines()]
